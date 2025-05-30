fc2d88bd3acb8362498552b1e2f653ef92d2d3a0
NathanDalbert <dalbert.nathan@gmail.com>
2025-05-30 18:58:41 -0300

    feat(order): criar order entity

--- src/order/order-item.entity/order-item.entity.ts
+++ src/order/order-item.entity/order-item.entity.ts
@@ -1,27 +1,26 @@
 // src/order/order-item.entity.ts
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { Product } from '../../product/product-entity/product.entity'; // Certifique-se que o caminho está correto
-import { Order } from '../order.entity/order.entity'; // Certifique-se que o caminho está correto
+import { Product } from '../../product/product-entity/product.entity';
+import { Order } from '../order.entity/order.entity';
 
-@Entity('order_items') // É uma boa prática nomear explicitamente a tabela
+@Entity('order_items') 
 export class OrderItem {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  // Relação com a entidade Product
-  @ManyToOne(() => Product, /* product => product.orderItems, // Opcional: lado inverso da relação */ { eager: true, nullable: false })
-  @JoinColumn({ name: 'product_id' }) // Especifica que a coluna 'product_id' nesta tabela é a FK
-  product: Product; // Esta é a propriedade da relação que armazena o objeto Product
+ 
+  @ManyToOne(() => Product, { eager: true, nullable: false })
+  @JoinColumn({ name: 'product_id' }) 
+  product: Product;
 
-  // Coluna da Chave Estrangeira (FK)
-  @Column({ name: 'product_id' }) // Esta é a coluna no banco de dados que armazena o ID do produto
+  @Column({ name: 'product_id' })
   productId: string;
 
   @Column()
   quantity: number;
 
   @Column('decimal', { precision: 10, scale: 2 })
-  pricePerUnit: number; // Preço do produto no momento da compra
+  pricePerUnit: number; 
 
   @ManyToOne(() => Order, order => order.items)
   order: Order;
