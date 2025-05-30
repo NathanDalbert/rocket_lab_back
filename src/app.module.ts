06bc29a0d2918027f4fe0005fc43a77469bf2b15
NathanDalbert <dalbert.nathan@gmail.com>
2025-05-30 13:21:47 -0300

    chore: Configura conexão TypeORM SQLite no AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { CartController } from '../src/cart/cart.controller';
+import { OrderController } from '../src/order/order.controller';
+import { OrderService } from '../src/order/order.service';
+import { ProductController } from '../src/product/product.controller';
+import { ProductService } from '../src/product/product.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CartModule } from './cart/cart.module';
+import { OrderModule } from './order/order.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ProductModule, CartModule, OrderModule],
+  controllers: [AppController, ProductController, CartController, OrderController],
+  providers: [AppService, ProductService, OrderService],
 })
 export class AppModule {}
