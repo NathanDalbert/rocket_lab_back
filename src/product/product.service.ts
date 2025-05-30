cb8d9c3ebc55dfd8096871f57c44a0d97f5797ee
NathanDalbert <dalbert.nathan@gmail.com>
2025-05-30 13:31:04 -0300

    feat(produto): Implementa operações CRUD básicas no ProductService

---
+++ src/product/product.service.ts
@@ -0,0 +1,50 @@
+
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateProductDto } from './DTO/create-product.dto';
+import { UpdateProductDto } from './DTO/update-product.dto';
+import { Product } from './product-entity/product.entity';
+
+@Injectable()
+export class ProductService {
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto): Promise<Product> {
+    const newProduct = this.productRepository.create(createProductDto);
+    return this.productRepository.save(newProduct);
+  }
+
+  async findAll(): Promise<Product[]> {
+    return this.productRepository.find();
+  }
+
+  async findOne(id: string): Promise<Product> {
+    const product = await this.productRepository.findOneBy({ id });
+    if (!product) {
+      throw new NotFoundException(`Product with ID "${id}" not found`);
+    }
+    return product;
+  }
+
+  async update(id: string, updateProductDto: UpdateProductDto): Promise<Product> {
+    const product = await this.productRepository.preload({
+      id: id,
+      ...updateProductDto,
+    });
+    if (!product) {
+      throw new NotFoundException(`Product with ID "${id}" not found`);
+    }
+    return this.productRepository.save(product);
+  }
+
+  async remove(id: string): Promise<void> {
+    const result = await this.productRepository.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`Product with ID "${id}" not found`);
+    }
+  }
+}
