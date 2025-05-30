43a373ee51da7c00ff71e4cd3aadfe2bc5d723d7
NathanDalbert <dalbert.nathan@gmail.com>
2025-05-30 18:50:12 -0300

    feat: adicionar modulos neccesarios ao app.modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,26 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
-import { CartController } from '../src/cart/cart.controller';
-import { OrderController } from '../src/order/order.controller';
-import { OrderService } from '../src/order/order.service';
-import { ProductController } from '../src/product/product.controller';
-import { ProductService } from '../src/product/product.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CartModule } from './cart/cart.module';
+import { CartModule } from './cart/cart.module'; // <<--- VERIFIQUE ESTE CAMINHO
 import { OrderModule } from './order/order.module';
 import { ProductModule } from './product/product.module';
+// ... outras importações de entidade se você as colocou aqui
 
 @Module({
-  imports: [ProductModule, CartModule, OrderModule],
-  controllers: [AppController, ProductController, CartController, OrderController],
-  providers: [AppService, ProductService, OrderService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    ProductModule,
+    CartModule, // <<--- E AQUI
+    OrderModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
