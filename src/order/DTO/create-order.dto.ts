7b5653ccef7d87e84a31f74ec598095b4277467b
NathanDalbert <dalbert.nathan@gmail.com>
2025-05-31 11:22:23 -0300

    docs(order): adicionar documentação Swagger no DTO de criação de pedido

--- src/order/DTO/create-order.dto.ts
+++ src/order/DTO/create-order.dto.ts
@@ -1,16 +1,21 @@
 // src/order/dto/create-order.dto.ts
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
 
 export class CreateOrderDto {
+  @ApiProperty({
+    example: '0b23fba7-d7f9-4e76-8cc0-4d03b1e22f7a',
+    description: 'ID do carrinho a partir do qual o pedido será criado'
+  })
   @IsNotEmpty()
   @IsUUID()
   cartId: string;
 
-  // Adicione outros campos necessários, como informações de pagamento, endereço, etc.
-  // @IsString()
-  // @IsOptional()
-  // paymentMethodId?: string; // Exemplo
-
+  @ApiProperty({
+    example: 'Rua das Laranjeiras, 123 - Salvador/BA',
+    description: 'Endereço de entrega',
+    required: false
+  })
   @IsString()
   @IsOptional()
   shippingAddress?: string;
